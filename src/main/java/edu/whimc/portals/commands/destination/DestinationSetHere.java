3d2540ce062ebe6f7a5ca11f2f86e21e9fac667b
emicb <emicb55@gmail.com>
2021-10-08 10:59:18 -0500

    Documented DestinationSetHere.java

--- src/main/java/edu/whimc/portals/commands/destination/DestinationSetHere.java
+++ src/main/java/edu/whimc/portals/commands/destination/DestinationSetHere.java
@@ -12,8 +12,20 @@ import edu.whimc.portals.commands.AbstractSubCommand;
 import edu.whimc.portals.utils.Messenger;
 import edu.whimc.portals.utils.Messenger.ReplaceMessage;
 
+/**
+ * The command to set the destination of the portal to the user's current location using
+ * the name of the portal.
+ * Command: "/destination sethere"
+ */
 public class DestinationSetHere extends AbstractSubCommand {
 
+    /**
+     * Constructs a DestinationSetHere.
+     *
+     * @param plugin the instance of the plugin.
+     * @param baseCommand the base command keyword.
+     * @param subCommand the subcommand keyword.
+     */
     public DestinationSetHere(Main plugin, String baseCommand, String subCommand) {
         super(plugin, baseCommand, subCommand);
         super.description("Sets the destination of a portal to your current location using the name of the portal");
@@ -21,9 +33,18 @@ public class DestinationSetHere extends AbstractSubCommand {
         super.requiresPlayer();
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * Set the destination of the current portal to the player's location using the name
+     * of the portal.
+     */
     @Override
     protected boolean onCommand(CommandSender sender, String[] args) {
+        // get the current portal
         Portal portal = Portal.getPortal(args[0]);
+
+        // notify if portal does not exist and abort command execution
         if (portal == null){
             Messenger.msg(sender, ReplaceMessage.PORTAL_DOES_NOT_EXIST, args[0]);
             return true;
@@ -33,18 +54,22 @@ public class DestinationSetHere extends AbstractSubCommand {
         Player player = (Player) sender;
         Destination dest = Destination.getDestination(destName);
 
+        // create a new destination if none exists, otherwise set the existing destination
         if (dest == null) {
             dest = Destination.createDestination(plugin, destName, player.getLocation());
         } else {
             dest.setLocation(player.getLocation());
         }
 
-
+        // set the new destination of the portal
         portal.setDestination(dest);
         Messenger.msg(sender, ReplaceMessage.DESTINATION_SETHERE, portal.getName(), portal.getName());
         return true;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected List<String> onTabComplete(CommandSender sender, String[] args) {
         return Portal.getTabCompletedPortals(args[0]);
